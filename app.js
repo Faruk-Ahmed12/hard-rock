5c591ad6358833c731af723ba233cf580cbfacad
Faruk-Ahmed12 <techwithfaruk@gmail.com>
2021-02-12 11:44:54 -0800

    added

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const searchSong = () => {
     const searchText = document.getElementById('search-field').value;
-    const url = `https://api.lyrics.ovh/suggest/$(searchText)`
+    const url = `https://api.lyrics.ovh/suggest/${searchText}`
     fetch(url)
         .then(res => res.json())
         .then(data => displaySongs(data.data));
-
+        // .catch(error => displayErrorMsg(error));
 }
 
 const displaySongs = songs => {
@@ -16,13 +16,38 @@ const displaySongs = songs => {
         songDiv.innerHTML = `
             <div class="col-md-9">
                 <h3 class="lyrics-name">${song.title}</h3>
-                <p class="author lead">Album by <span>Washed Out</span></p>
+                <p class="author lead">Album by <span>${song.artist.name}</span></p>
+
+                <p class="author lead">Song Id : <span>${song.artist.id}</span></p>
+
+                <audio controls>
+                    <source src="${song.preview}" type="audio/ogg">
+                </audio>
+
+                <p class="author lead">Full Song Duration : <span>${song.duration/60 + ' Minutes' }</span></p>
             </div>
                 <div class="col-md-3 text-md-right text-center">
-                <button class="btn btn-success">Get Lyrics</button>
+                <button onclick="getLyrics('${song.artist.name}','${song.title}')" class="btn btn-success">Get Lyrics</button>
             </div>
         `;
         songContainer.appendChild(songDiv);
-        console.log(songContainer);
     })
+}
+
+const getLyrics = (artist,title) => {
+    const url = `https://api.lyrics.ovh/v1/${artist}/${title}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayLyrics(data.lyrics))
+
+}
+
+const displayLyrics = lyrics => {
+    const lyricsDiv = document.getElementById('song-lyrics');
+    lyricsDiv.innerText = lyrics
+}
+
+const displayErrorMsg = error => {
+    const errorTagH3 = document.getElementById('error-msg')
+    errorTagH3.innerText = error;
 }
